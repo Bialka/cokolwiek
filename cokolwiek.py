88028c61c1a54e3c42978d7f9fb72a2c10b69664
Kasia <kbialolecka@gmail.com>
2018-07-17 00:35:28 +0200

    wypisywanie katalogów

--- cokolwiek.py
+++ cokolwiek.py
@@ -6,6 +6,8 @@ import os
 print ("Zaczynam")
 if __name__ == "__main__":
     for x in os.walk("do_zrobienia"):
+        print ("\t", x[0])
+        print ("\t", x[1])
         for nazwa_pliku in x[2]:
             if nazwa_pliku.endswith(".mp3"):
                 print("\t", nazwa_pliku)
