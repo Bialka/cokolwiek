7d8b94c278fd53f7f90d23b9a00946adeeaa5e99
Kasia <kbialolecka@gmail.com>
2018-08-30 21:44:29 +0200

    stworzenie słownika

--- cokolwiek.py
+++ cokolwiek.py
@@ -5,11 +5,17 @@ import os
 
 print ("Zaczynam")
 if __name__ == "__main__":
+    slownik = {}
+    licznik_plikow = 0
+    licznik_katalogow = 0
     for x in os.walk("do_zrobienia"):
-        juz_wyprintowany = False
+        nowa_lista = []
         for nazwa_pliku in x[2]:
             if nazwa_pliku.endswith(".mp3"):
-                if not juz_wyprintowany:
-                    print (x[0])
-                    juz_wyprintowany = True
-                print("\t", nazwa_pliku)
+                nowa_lista.append(nazwa_pliku)
+                licznik_plikow += 1
+                licznik_katalogow += 1
+            if nowa_lista:
+                slownik[(x[0])] = nowa_lista
+    print(slownik)
+    print("Znaleziono {} plików w {} katalogach.".format(licznik_plikow, licznik_katalogow))            
