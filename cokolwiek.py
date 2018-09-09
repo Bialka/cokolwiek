0a2a7b62601e7ee2a3b6885c7c46afe844a5f915
Kasia <kbialolecka@gmail.com>
2018-09-09 20:09:54 +0200

    poprawa licznika katalogow

--- cokolwiek.py
+++ cokolwiek.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
     licznik_plikow = 0
     licznik_katalogow = 0
     for x in os.walk("do_zrobienia"):
+        licznik_katalogow += 1
         nowa_lista = []
         for nazwa_pliku in x[2]:
             if nazwa_pliku.endswith(".mp3"):
                 nowa_lista.append(nazwa_pliku)
                 licznik_plikow += 1
-                licznik_katalogow += 1
-            if nowa_lista:
-                slownik[(x[0])] = nowa_lista
+        if nowa_lista:
+            slownik[(x[0])] = nowa_lista
     print(slownik)
     print("Znaleziono {} plików w {} katalogach.".format(licznik_plikow, licznik_katalogow))            
