a2db140ae85e6c48ab3c6ae06c557b791e77ec29
Bialka <kbialolecka@gmail.com>
2020-01-20 23:21:42 +0100

    Utworzenie listy plików muzycznych w klasie Dir.

--- dir_class.py
+++ dir_class.py
@@ -5,13 +5,18 @@ import os
 class Dir:
     def __init__(self, base_dir):
         self.base_dir = base_dir
+        self.music_files = self.get_music_files()
 
-    def is_music_files_dir(self):
+    def get_music_files(self):
+        music_files = []
         for item in os.listdir(self.base_dir):
             pth = os.path.join(self.base_dir, item)
             if os.path.isfile(pth) and self.is_music_file(pth):
-                return True
-        return False
+                music_files.append(item)
+        return music_files
+
+    def is_music_files_dir(self):
+        return bool(self.music_files)
 
     def is_music_file(self, pth):
         return pth.endswith(".mp3")
@@ -22,9 +27,4 @@ class Dir:
         return " - | {0: <50}|{1: <25} | {2: >3}\n".format(self.base_dir, target_dir_name, music_files_count)
 
     def get_music_files_count(self):
-        files_count = 0
-        for item in os.listdir(self.base_dir):
-            pth = os.path.join(self.base_dir, item)
-            if os.path.isfile(pth) and self.is_music_file(pth):
-                files_count += 1
-        return files_count
+        return len(self.music_files)
