40d430c26b643eea78107fdd992223ae7cab5bb5
Bialka <kbialolecka@gmail.com>
2020-01-23 21:51:57 +0100

    Zamiana nazw zmiennych i poprawienie .strip() przy liście plików.

--- cokolwiek.py
+++ cokolwiek.py
@@ -7,46 +7,45 @@ import shutil
 
 import dir_class
 
-def zbierz_dane_o_mp3(katalog):
+def zbierz_dane_o_mp3(directory):
     music_files_dirs = []
-    for x in os.walk(katalog):
+    for x in os.walk(directory):
         cos_dir = dir_class.Dir(x[0])
         if cos_dir.is_music_files_dir():
             music_files_dirs.append(cos_dir)
     return music_files_dirs
 
 
-def zapis(music_files_dirs, sciezka):
-    with open(sciezka, 'w') as f:
+def zapis(music_files_dirs, path):
+    with open(path, 'w') as f:
         for cos_dir in music_files_dirs:
             f.write(cos_dir.get_control_string())
 
 
-def obrobka_plikow(sciezka_pliku):
-    with open(sciezka_pliku, 'r') as f:
+def obrobka_plikow(file_path):
+    with open(file_path, 'r') as f:
         x = f.read().strip()
         for ln in x.split("\n"):
             ln = dir_class.Dir.parse_control_string(ln)
-            cos_dir = dir_class.Dir(ln[1].strip())
-            if ln[0].strip() == "u":
-                cos_dir.delete(ln[1].strip())
-            elif ln[0].strip() == "p":
-                cos_dir.move(ln[1].strip(), ln[2].strip())
-            print(ln)
-    os.remove(sciezka_pliku)
+            cos_dir = dir_class.Dir(ln[1])
+            if ln[0] == "u":
+                cos_dir.delete(ln[1])
+            elif ln[0] == "p":
+                cos_dir.move(ln[1], ln[2])
+    os.remove(file_path)
 
 
 if __name__ == "__main__":
     print("Zaczynam")
-    sciezka_pliku = 'cokolwiek.txt'
-    obecny_katalog = "do_zrobienia"
-    katalog_docelowy = "zrobione"
+    file_path = 'cokolwiek.txt'
+    current_dir = "do_zrobienia"
+    destination_dir = "zrobione"
 
-    if not os.path.exists(katalog_docelowy):
-        os.makedirs(katalog_docelowy)
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
 
-    if os.path.isfile(sciezka_pliku):
-        obrobka_plikow(sciezka_pliku)
+    if os.path.isfile(file_path):
+        obrobka_plikow(file_path)
     else:
         music_files_dirs = zbierz_dane_o_mp3("do_zrobienia")
-        zapis(music_files_dirs, sciezka_pliku)
+        zapis(music_files_dirs, file_path)
