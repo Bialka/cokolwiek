e7aa34d06999002b18763048e6b31521f99d3a6b
Bialka <kbialolecka@gmail.com>
2020-08-02 11:42:54 +0200

    Poprawa formatowania stringów kontrolnych funkcji edit

--- test.py
+++ test.py
@@ -5,13 +5,16 @@ import os
 import shutil
 
 class TestStringMethods(unittest.TestCase):
-    file_name = "cokolwiek.txt"
+    file_name1 = "cokolwiek.txt"
+    file_name2 = "dane_plikow.txt"
     to_do_path = "do_zrobienia"
     destination_path = "zrobione"
     backup_path = "backup"
     def setUp(self):
-        if os.path.exists(self.file_name):
-            os.remove(self.file_name)
+        if os.path.exists(self.file_name1):
+            os.remove(self.file_name1)
+        if os.path.exists(self.file_name2):
+            os.remove(self.file_name2)
         shutil.rmtree(self.destination_path, ignore_errors=True)
         os.mkdir(self.destination_path)
         shutil.rmtree(self.to_do_path, ignore_errors=True)
