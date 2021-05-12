52a4c0bdf214c3bfb3a8f89d578195871d89b78c
Bialka <kbialolecka@gmail.com>
2021-05-12 14:42:06 +0200

    Poprawa odczytywania argumentów.

--- ogarniacz_mp3.py
+++ ogarniacz_mp3.py
@@ -16,16 +16,12 @@ def get_music_dirs(dir_path):
 
 def reading_arguments_from_terminal():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--p2o")
-    parser.add_argument("--o2w")
-    parser.add_argument("--w2g")
+    parser.add_argument("--p2o", action="store_true")
+    parser.add_argument("--o2w", action="store_true")
+    parser.add_argument("--w2g", action="store_true")
     args = parser.parse_args()
-    if args == "--p2o":
-        downloaded_to_processing()
-    if args == "--o2w":
-        processing_to_verification()
-    if args == "--w2g":
-        verification_to_ready()
+    print(args)
+    return args
 
 
 def downloaded_to_processing():
@@ -43,4 +39,18 @@ def verification_to_ready():
 if __name__ == "__main__":
     dir_path = "/home/katrzyna/Documents/cokolwiek/tests/TestData"
     get_music_dirs(dir_path)
-    reading_arguments_from_terminal()
+    if reading_arguments_from_terminal().p2o == True:
+        downloaded_to_processing()
+    if reading_arguments_from_terminal().o2w == True:
+        processing_to_verification()
+    if reading_arguments_from_terminal().w2g == True:
+        verification_to_ready()
+    if reading_arguments_from_terminal().p2o and reading_arguments_from_terminal().o2w == True:
+        downloaded_to_processing()
+        processing_to_verification()
+        print("hello")
+    if reading_arguments_from_terminal().p2o and reading_arguments_from_terminal().o2w and reading_arguments_from_terminal().w2g == False:
+        print("hello")
+        downloaded_to_processing()
+        processing_to_verification()
+        verification_to_ready()
