8e1a8190541789c85c39c168272b6d967f2df298
Bialka <kbialolecka@gmail.com>
2021-06-12 18:59:39 +0200

    Napisanie funkcji downloaded_to_processing.

--- ogarniacz_mp3.py
+++ ogarniacz_mp3.py
@@ -4,6 +4,7 @@
 import os
 import classes
 import argparse
+from zipfile import ZipFile
 
 
 def get_music_dirs(dir_path):
@@ -20,12 +21,18 @@ def reading_arguments_from_terminal():
     parser.add_argument("--o2w", action="store_true")
     parser.add_argument("--w2g", action="store_true")
     args = parser.parse_args()
-    print(args)
     return args
 
 
-def downloaded_to_processing():
-    pass
+def downloaded_to_processing(source_dir_path, target_dir_path): #ścieżka do kat jako argument, znaleźć w kat wszystkie pliki .zip, rozpakować je do kat "do obróbki"
+    for dir_path, sub_dirs, files in os.walk(source_dir_path):
+        for f in files:
+            if f.endswith(".zip"):
+                source_file_path = os.path.join(dir_path, f)
+                target_subdir_path = os.path.join(target_dir_path, f.replace(".zip", ""))
+                with ZipFile(source_file_path, "r") as zf:
+                    zf.extractall(target_subdir_path)
+                os.remove(source_file_path)
 
 
 def processing_to_verification():
@@ -38,10 +45,12 @@ def verification_to_ready():
 
 if __name__ == "__main__":
     dir_path = "/home/katrzyna/Documents/cokolwiek/tests/TestData"
-    get_music_dirs(dir_path)
+    from_dir_path = "/home/katrzyna/Documents/cokolwiek/tests/TestData/Pobrane"
+    final_dir_path = "/home/katrzyna/Documents/cokolwiek/tests/TestData/Do Obróbki"
+    #get_music_dirs(dir_path)
     args = reading_arguments_from_terminal()
     if args.p2o:
-        downloaded_to_processing()
+        downloaded_to_processing(from_dir_path, final_dir_path)
     if args.o2w:
         processing_to_verification()
     if args.w2g:
