40571fe0114aaa810b72b03c44670c4706d4a22b
Bialka <kbialolecka@gmail.com>
2021-07-31 19:17:45 +0200

    Tworzenie katalogu tymczasowego.

--- ogarniacz_mp3.py
+++ ogarniacz_mp3.py
@@ -8,6 +8,7 @@ from zipfile import ZipFile, BadZipfile
 import re
 import shutil
 import subprocess
+import tempfile
 
 
 def get_music_dirs(dir_path):
@@ -84,14 +85,16 @@ def adjust_bitrates(processing_dir_path):
                 music_file = classes.MusicFile(file_path)
                 bitrate = int(music_file.bit_rate)
                 if bitrate > 142000:
-                    output_path = os.path.join(dir_path, "tym" + file_name)
-                    proc = subprocess.run(["ffmpeg", "-i", file_path, "-vn", "-ar", "44100", "-ac", "2", "-b:a", "142000",
-                                           output_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                    if proc.returncode == 0:
-                        shutil.copy(output_path, file_path)
-                        os.remove(output_path)
-                    else:
-                        print(proc.stderr.decode())
+                    with tempfile.TemporaryDirectory() as temp_dir_name:
+                        print("Created temporary directory", temp_dir_name)
+                        output_path = os.path.join(temp_dir_name, file_name)
+                        proc = subprocess.run(["ffmpeg", "-i", file_path, "-vn", "-ar", "44100", "-ac", "2", "-b:a", "142000",
+                                               output_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        if proc.returncode == 0:
+                            shutil.copy(output_path, file_path)
+                            os.remove(output_path)
+                        else:
+                            print(proc.stderr.decode())
 
 
 def downloaded_to_processing(source_dir_path, target_dir_path): #ścieżka do kat jako argument, znaleźć w kat wszystkie pliki .zip, rozpakować je do kat "do obróbki"
